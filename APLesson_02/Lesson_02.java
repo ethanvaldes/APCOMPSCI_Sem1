609850ba111a91c8f3f9d9c625b9f267543ed01d
ethanvaldes <ethanv@att.net>
2016-09-06 09:20:28 -0700

    Added Files

---
+++ APLesson_02/Lesson_02.java
@@ -0,0 +1,26 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		int x = 3;
+		int y = 2;
+		int s = 3 * 2;
+		
+		System.out.println(x + " multiplied by " + y + " is " + s);
+
+		String name = "Ethan Valdes";
+		String address = "615 E Circle Dr";
+		String city = "Solana Beach";
+		String state = "CA";
+		int zip = 92130;
+		
+		System.out.println (name);
+		System.out.println (address);
+		System.out.println (city + "," + state + " " + zip);
+		
+		int l = 10;
+		int h = 5;
+		int w = 2;
+		
+	}
+}
---
+++ APLesson_02/Lesson_02_example.java
@@ -0,0 +1,8 @@
+public class Lesson_02_example
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);	
+	}
+}
