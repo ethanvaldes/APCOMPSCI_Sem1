14693bb91bb0671e986a15bd4c022a1e78ff9698
ethanvaldes <ethanv@att.net>
2016-09-06 07:50:43 -0700

    Added Files

---
+++ APLesson_01/crown.java
@@ -0,0 +1,21 @@
+public class crown
+{
+	public static void main (String []args)
+	{
+		System.out.println("                      /|\\");
+		System.out.println("                      \\|/");
+		System.out.println("                    . \' ` .");
+		System.out.println("*             *  . '   *    ` .  *             *");
+		System.out.println("` \\          / \\\'     / \\      `/ \\          / \'");
+		System.out.println(" `  \\       /   \\    /   \\     /   \\       /  \'");
+		System.out.println("   `    \\\'/       \\/       \\ /       \\`/    \'");
+		System.out.println("    `_____________________________________ '");
+		System.out.println("     _____________________________________");
+		System.out.println("     _____________________________________");
+		System.out.println("     |                                   |");
+		System.out.println("     *     *     *     *     *     *     *");
+		System.out.println("     |___________________________________|");
+		System.out.println("     _____________________________________");
+	
+	}
+}
---
+++ APLesson_01/ghost.java
@@ -0,0 +1,23 @@
+public class ghost
+{
+	public static void main (String []args)
+	{
+		System.out.println("                .---.");
+		System.out.println("              /       \\");
+		System.out.println("---- _       |  O   O  |       _ ---");
+		System.out.println("\\       ~-.  `.       .'  .-~      /");
+		System.out.println(" \\          ~-_>     <_-~         /");
+		System.out.println("   \\                             /");
+		System.out.println("     \\                         /");
+		System.out.println("       \\                     /");
+		System.out.println("         \\                 /");
+		System.out.println("             \\         /");
+		System.out.println("               \\       \\");
+		System.out.println("                 \\      \\");
+		System.out.println("                   \\     \\");
+		System.out.println("                       \\   \\");
+		System.out.println("                         \\ /");
+		System.out.println("                          V");
+	
+	}
+}
