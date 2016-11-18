20a88ed848ac2ea1d0195f93a22f880890b1c532
ethanvaldes <ethanv@att.net>
2016-11-18 09:19:30 -0800

    i luh u

---
+++ APLesson_08/center.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class center
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a word: ");
+		String word1 = kb.next;
+		System.out.println("Please enter a word: ");
+		String word2 = kb.next;
+		System.out.println("Please enter a word: ");
+		String word3 = kb.next;
+		makeCenter();
+	}
+	
+	public static String makeCenter(String word)
+	{
+		
+	}
+}
