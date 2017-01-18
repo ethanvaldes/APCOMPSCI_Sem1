7426773f4f2ab4e2eb87c6dc7ef0c0f734b54e1c
ethanvaldes <ethanv@att.net>
2017-01-18 09:16:00 -0800

    kandy

---
+++ APLesson_11/randomnumbers.java
@@ -0,0 +1,20 @@
+public class randomnumbers
+{
+	public static void main(String[]args)
+	{
+		int[][]nums = new int[4][4];
+		for(int i=0; i<nums.length; i++)
+			for(int j=0; j<nums[i].length; j++)
+				nums[i][j] = (int)(Math.random()*10);
+			
+		for(int i=0; i<nums.length; i++)
+		{
+			for(int j=0; j<nums[i].length; j++)
+			{
+				System.out.print(nums[i][j]);
+			}
+			
+			System.out.println();
+		}
+	}
+}
